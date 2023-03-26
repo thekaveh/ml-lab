c3a8f6efc881933b8872b5d76147338cfaefc221
Kaveh <kaveh.razavi@gmail.com>
2023-03-25 23:30:59 -0400

    adds mnist mini-project

---
+++ mnist/consts.py
@@ -0,0 +1,10 @@
+class Consts:
+    ETHA: float = 0.1
+    DEVICE: str = "cpu"
+    NUM_EPOCHS: int = 50
+    OPTIMIZER_ALG = "sgd"
+    DROPOUT_P: float = 0.25
+    FEATURES_SIZE_OUT: int = 10
+    MINI_BATCH_SIZE: int = 60000
+    FEATURES_SIZE_IN: int = 28 * 28
+    DATASET_SAMPLING_RATIO: float = 1.0
